431a1daeddf109b3cca7e2a6be9f9d875c9b4618
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-07-04 21:39:08 +0500

    Initialized Repo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MERN</a>
+      </header>
+      <main>list the products here</main>
+    </div>
+  );
+}
+
+export default App;
