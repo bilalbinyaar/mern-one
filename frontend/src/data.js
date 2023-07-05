f74745c59cbf959cb76d3bbbc6c12c94ca26ffd0
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-07-05 19:15:51 +0500

    data.js file added

---
+++ frontend/src/data.js
@@ -0,0 +1,61 @@
+const data = {
+  products: [
+    // OBJECT # 1
+    {
+      name: 'Product One',
+      slug: 'product-one',
+      category: 'Category One',
+      image: '/images/p1.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Brand One',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'This is product one',
+    },
+
+    // OBJECT # 2
+    {
+      name: 'Product Two',
+      slug: 'product-two',
+      category: 'Category Two',
+      image: '/images/p2.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Brand One',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'This is product two',
+    },
+
+    // OBJECT # 3
+    {
+      name: 'Product Three',
+      slug: 'product-three',
+      category: 'Category Two',
+      image: '/images/p3.png',
+      price: 120,
+      countInStock: 10,
+      brand: 'Brand One',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'This is product three',
+    },
+
+    // OBJECT # 4
+    {
+      name: 'Product Four',
+      slug: 'product-four',
+      category: 'Category One',
+      image: '/images/p4.png',
+      price: 150,
+      countInStock: 10,
+      brand: 'Brand One',
+      rating: 4.7,
+      numReviews: 10,
+      description: 'This is product four',
+    },
+  ],
+};
+
+export default data;
